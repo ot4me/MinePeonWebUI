4024267717cca5b2facffc353a36d2d539dfea37
MineForeman <neil@mineforeman.com>
2013-05-24 07:02:48 +0100

    Split HTML sections to files

---
+++ menu.php
@@ -0,0 +1,23 @@
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="http://mineforeman.com/minepeon/">MinePeon</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+                          <li><a href="/">Graphs</a></li>
+              <li><a href="pools.php">Pools</a></li>
+                          <li><a href="miner.php">Stats</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+                          <li><a href="#license">License</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
